fe57e82decb5f850c81843f88b799f8d88fd008f
Pratibha <pratibha.tomar13@gmail.com>
2020-09-24 10:10:26 +0200

    removed all console

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thrusday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
-  console.log(response);
-  console.log(response.data.name);
   let cityElement = document.querySelector("#city-country");
   let tempElement = document.querySelector("#temp");
   let descriptionElement = document.querySelector("#description");
   let maxElement = document.querySelector("#max-temp");
   let minElement = document.querySelector("#min-temp");
   let feelsElement = document.querySelector("#feels-like");
+  let dateElement = document.querySelector("#date-month");
   cityElement.innerHTML = response.data.name;
   tempElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
   maxElement.innerHTML = Math.round(response.data.main.temp_max) + `°C`;
   minElement.innerHTML = Math.round(response.data.main.temp_min) + `°C`;
   feelsElement.innerHTML = Math.round(response.data.main.feels_like) + `°C`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "26c70cc759f2dda82240508e33d14cb9";
